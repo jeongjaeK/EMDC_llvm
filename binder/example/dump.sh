a4a8ef3a9df2dc69126240096d4ea3569f6bb941
sungin-h <sungin4394@gmail.com>
2021-11-03 17:08:14 +0900

    clean up binder/example

---
+++ binder/example/dump.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir -p tmp
+section_list=`cat ./section_list`
+
+#objcopy --update-section __CLANG_OFFLOAD_BUNDLE__sycl-spir64_fpga=/dev/null t1
+
+for i in $section_list;do
+	objcopy --dump-section $i=tmp$i t0
+done
+#for i in $section_list;do
+#	objcopy --update-section $i=/dev/null t1
+#done
+#for i in $section_list;do
+#	echo $i
+#	objcopy --change-section-lma $i-104050 t1 t2
+#done
+#for i in $section_list;do
+#	objcopy --update-section $i=tmp$i t4
+#done
+
